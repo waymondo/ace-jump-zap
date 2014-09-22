0f709b4708d855dc568a47e1afa52c4aba7d2272
justin talbott <justin@waymondo.com>
2014-09-22 00:13:43 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+### Ace Jump Zap
+
+`ace-jump-zap` is the functionality of [`zap-to-char`](http://www.emacswiki.org/emacs/ZapToChar) and [`zap-up-to-char`](http://www.emacswiki.org/emacs/ZapUpToChar) using [`ace-jump-mode`](https://github.com/winterTTr/ace-jump-mode). In a nutshell, you may trigger a key-binding to launch `ace-jump-char-mode` to vaporize all characters between the cursor and the selected character.
+
+<!-- #### Installation -->
+
+<!-- Install from [MELPA](melpa.milkbox.net) with `package-install ace-jump-zap`, or drop `ace-jump-mode.el` and `ace-jump-zap.el` into your load path. -->
+
+#### Usage
+
+Bind `ace-jump-zap-up-to-char` or `ace-jump-zap-to-char` to the key-binding of your heart's desire and zap away.
---
+++ ace-jump-zap.el
@@ -0,0 +1,61 @@
+;;; ace-jump-zap.el --- Character zapping, `ace-jump-mode` style
+
+;; Copyright (C) 2014  justin talbott
+
+;; Author: justin talbott <justin@waymondo.com>
+;; Keywords: convenience, tools, extensions
+;; URL: https://github.com/waymondo/ace-jump-zap
+;; Version: 0.0.1
+;; Package-Requires: ((ace-jump-mode "1.0"))
+;;
+;;; Code:
+
+(require 'ace-jump-mode)
+
+(defvar ajz/zapping nil)
+(defvar ajz/to-char nil)
+
+(defun ajz/maybe-zap-start ()
+  (when ajz/zapping
+    (call-interactively 'set-mark-command)))
+
+(defun ajz/maybe-zap-end ()
+  (when ajz/zapping
+    (when ajz/to-char (forward-char))
+    (call-interactively 'delete-region)
+    (deactivate-mark))
+  (ajz/reset))
+
+(defun ajz/reset ()
+  (setq ajz/zapping nil)
+  (setq ajz/to-char nil))
+
+(defun ajz/keyboard-reset ()
+  (interactive)
+  (ajz/reset)
+  (ace-jump-done))
+
+(add-hook 'ace-jump-mode-before-jump-hook #'ajz/maybe-zap-start)
+(add-hook 'ace-jump-mode-end-hook #'ajz/maybe-zap-end)
+
+;;;###autoload
+(defun ace-jump-zap-up-to-char ()
+  "Call `ace-jump-char-mode' and zap all characters
+up to the selected character."
+  (interactive)
+  (let ((ace-jump-mode-scope 'window))
+    (setq ajz/zapping t)
+    (call-interactively 'ace-jump-char-mode)
+    (define-key overriding-local-map (kbd "C-g") 'ajz/keyboard-reset)
+    (define-key overriding-local-map [t] 'ajz/keyboard-reset)))
+
+;;;###autoload
+(defun ace-jump-zap-to-char ()
+  "Call `ace-jump-char-mode' and zap all characters
+up to and including the selected character."
+  (interactive)
+  (setq ajz/to-char t)
+  (ace-jump-zap-up-to-char))
+
+(provide 'ace-jump-zap)
+;;; ace-jump-zap.el ends here
